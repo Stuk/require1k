df59f06775147198094b414f4fece9403da01b44
Stuart Knightley <stuart@stuartk.com>
2014-11-29 11:52:02 -0800

    Combine resolve and getModule into one function. 963

--- require1k.js
+++ require1k.js
@@ -1,5 +1,14 @@
 /*jshint node:false, -W082, -W017 */
 R = (function (global, document, undefined) {
+    // Each module has the following properties (shorted to one letter to aid compression)
+    // - g: booleany, loadinG, truthy if this module has been requested for loading
+    //      before. Used to prevent the same module being loaded twice
+    // - l: string, Location, the url location of this module
+    // - t: string, Text, the text content of the module
+    // - e: booleany, Error, truthy if there was an error (probably a 404) loading the module
+    // - n: module object, Next, instead of using this module, use the object
+    //      pointed to by this property. Used for dependencies in other packages
+    // - exports, object, the exports of the module!
     var MODULES = {};
 
     // By using a named "eval" most browsers will execute in the global scope.
@@ -11,28 +20,15 @@ R = (function (global, document, undefined) {
         relativeElement = document[tmp]("a");
     document.head.appendChild(baseElement);
 
-    function resolve(base, relative, resolved) {
+    // `resolve` and `getModule` combined into one function to save bytes
+    function resolveAndGetModule(base, relative, resolved) {
         baseElement.href = base;
         // If the relative url begins with a letter (and not a "."), then it's
         // in node_modules
         relativeElement.href = relative.replace(/^(\w)/, "./node_modules/$1");
         resolved = relativeElement.href;
         baseElement.href = "";
-        return resolved;
-
-    }
-
-    // A module has the following properties (shorted to one letter to aid compression)
-    // - g: booleany, loadinG, truthy if this module has been requested for loading
-    //      before. Used to prevent the same module being loaded twice
-    // - l: string, Location, the url location of this module
-    // - t: string, Text, the text content of the module
-    // - e: booleany, Error, truthy if there was an error (probably a 404) loading the module
-    // - n: module object, Next, instead of using this module, use the object
-    //      pointed to by this property. Used for dependencies in other packages
-    // - exports, object, the exports of the module!
-    function getModule(location) {
-        return MODULES[location] = MODULES[location] || {l: location};
+        return (MODULES[resolved] = MODULES[resolved] || {l: resolved});
     }
 
     function deepLoad(module, callback, parentLocation, path, dep) {
@@ -63,7 +59,7 @@ R = (function (global, document, undefined) {
                 }
                 deps.map(function (dep) {
                     deepLoad(
-                        getModule(resolve(module.l, dep)),
+                        resolveAndGetModule(module.l, dep),
                         loaded,
                         // If it doesn't begin with a ".", then we're searching
                         // node_modules, so pass in the info to make this
@@ -80,7 +76,7 @@ R = (function (global, document, undefined) {
                 if (parentLocation) {
                     // Recurse up the tree trying to find the dependency
                     // (generating 404s on the way)
-                    deepLoad(module.n = getModule(resolve(parentLocation + (path += "../"), dep)), callback, parentLocation, path, dep);
+                    deepLoad(module.n = resolveAndGetModule(parentLocation + (path += "../"), dep), callback, parentLocation, path, dep);
                 } else {
                     callback(request, module);
                 }
@@ -98,7 +94,7 @@ R = (function (global, document, undefined) {
         if (!module[tmp]) {
             globalEval("(function(require,exports,module){"+module.t+"\n})")(
                 function require (id) {
-                    return getExports(MODULES[resolve(module.l, id)]);
+                    return getExports(resolveAndGetModule(module.l, id));
                 }, // require
                 module[tmp] = {}, // exports
                 module // module
@@ -109,7 +105,7 @@ R = (function (global, document, undefined) {
     }
 
     function R(id, callback) {
-        deepLoad(getModule(resolve(location, id)), function (err, module) {
+        deepLoad(resolveAndGetModule(location, id), function (err, module) {
             id = getExports(module);
             if (callback) {
                 callback(err, id);
